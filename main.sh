9f400d2c63ce19472cea1f60a1bf44029601c27f
nzo <enzomuller31@gmail.com>
2024-07-19 15:40:44 +0200

    Files reorganization second time

--- main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Do you want to install Nvidia driver ? [y/N] " response
-
-if [ $response == 'Y' ] || [ $response == 'y' ];
-then
-    echo ./nvidia.conf
-fi
--- paru.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 'Installing Paru'
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
