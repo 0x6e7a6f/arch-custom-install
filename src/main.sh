ec02d2b10eb1e61a82ab5ee6f557b0cbe201fb71
nzo <47951096+0x6e7a6f@users.noreply.github.com>
2024-07-20 21:34:51 +0200

    Can now install and choose a Wrapper. Directory of scripts are now fixed

--- src/main.sh
+++ src/main.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# NVIDIA Drivers
 read -p "Do you want to install Nvidia driver ? [y/N] " response
 
 if [ $response == 'Y' ] || [ $response == 'y' ];
 then
-    echo ./nvidia.conf
+    ./blocks/nvidia.sh
+fi
+
+
+# WRAPPER INSTALLATION
+read -p 'Which wrapping do you want to install ? \n1. The default option. For one who want to keep Pacman living\n2. Paru (for Rust lovers <3)\n3. Yay (The most popular Yogurt)' response
+
+if [ $response == '2' ] || [ $response == 'paru' ] || [ $response == 'Paru' ];
+then
+    echo 'Paru option selected. (I love you, my dear Rust Lover)'
+    ./blocks/paru.sh
+elif  [ $response == '3' ] || [ $response == 'yay' ] || [ $response == 'Yay' ];
+then
+    echo 'Yay option selected.'
+    ./blocks/yay.sh$
+else
+    echo 'Default option selected. Nothing done'
 fi
